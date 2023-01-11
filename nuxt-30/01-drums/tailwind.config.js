9d4a40a768d98fbd0d5f9bdba0c9f23cea624840
CricaDev <cristiancasallas998@gmail.com>
2023-01-11 18:43:30 -0500

    iniitial

---
+++ nuxt-30/01-drums/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+     "./components/**/*.{js,vue,ts}",
+     "./layouts/**/*.vue",
+     "./pages/**/*.vue",
+     "./plugins/**/*.{js,ts}",
+     "./app.vue",
+     "./nuxt.config.{js,ts}",
+     "content/**/*.md",
+ 
+     
+   ],
+   darkMode: 'class',
+   
+ }
+ 

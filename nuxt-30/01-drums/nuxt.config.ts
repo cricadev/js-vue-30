a465200d7955ff7a7a1cbe4fb329e91abca70053
CricaDev <cristiancasallas998@gmail.com>
2023-01-19 11:58:43 -0500

    bunch of changes

--- nuxt-30/01-drums/nuxt.config.ts
+++ nuxt-30/01-drums/nuxt.config.ts
@@ -9,4 +9,14 @@ export default defineNuxtConfig({
     injectPosition: 0,
     viewer: true,
   },
+  colorMode: {
+    preference: "system", // default value of $colorMode.preference
+    fallback: "light", // fallback value if not system preference found
+    hid: "nuxt-color-mode-script",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "-mode",
+    storageKey: "nuxt-color-mode",
+  },
 });
